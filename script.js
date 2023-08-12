89943eb973440cf5e9fe304ebffa52c8df1c40f1
Jeo <103911613+j30g@users.noreply.github.com>
2023-08-12 12:49:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+ function dis(val) {
+            document.getElementById("result").value += val
+        }
+
+function clr() {
+            document.getElementById("result").value = ""
+        }
+
+function onPressBackspace() {
+    result.value = result.value.substring(0, result.value.length - 1);
+}
+
+function myFunction(event) {
+            if (event.key == '0' || 
+                event.key == '1' || 
+                event.key == '2' || 
+                event.key == '3'|| 
+                event.key == '4' ||
+                event.key == '5' || 
+                event.key == '6' || 
+                event.key == '7'|| 
+                event.key == '8' || 
+                event.key == '9' || 
+                event.key == '+' || 
+                event.key == '-' || 
+                event.key == '*' || 
+                event.key == '/' || 
+                event.key == 'e')
+                document.getElementById("result").value += event.key;
+        }
+
+
+function solve() {
+            let x = document.getElementById("result").value;
+            let y = eval(x);
+            document.getElementById("result").value = y;
+        }
+  
